b27ce0cd51c30b69861eeabf1384fa5654e97301
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2023-03-12 21:18:13 +0530

    Creation of new storage class (Phase 0/4)
    * Shift storage backend to separate
      classes, like database
    * Add support for MyPHP Cloud API
    * Phase 0 - Creation of backend
      Phase 1 - Porting of filesystem
      backend
      Phase 2 - Implementing that
      Phase 3 - MyPHP Backend (after
      March 23rd 2023)
      Phase 4 - Config values for choosing
      adapter

---
+++ gurutracker/storage/base.py
@@ -0,0 +1,106 @@
+from abc import ABC, abstractmethod
+
+class StorageError(Exception):
+    pass
+
+
+class CannotLinkFileError(StorageError):
+    """When a storage adapter cannot link/associate a file, this error to be
+    raised.
+    """
+    def __init__(self, assignment, fp):
+        self.assignment = assignment
+        self.fp = fp
+        super().__init__("Failed to link file {} to {}".format(
+            assignment, fp))
+
+
+class CannotUninkFileError(StorageError):
+    """When a storage adapter cannot link/associate a file, this error to be
+    raised.
+    """
+    def __init__(self, assignment):
+        self.assignment = assignment
+        super().__init__("Failed to unlink files of {}".format(
+            assignment))
+
+
+class CannotGetFileError(StorageError):
+    """When a storage adapter cannot link/associate a file, this error to be
+    raised.
+    """
+    def __init__(self, assignment):
+        self.assignment = assignment
+        super().__init__("Failed to get files of {}".format(
+            assignment))
+
+class Base(ABC):
+    """This class is to be used as a base for all storage backends.
+
+    One must implement all the methods declared here, for a fully working
+    File Storage adapter.
+    """
+
+    @abstractmethod
+    def link_file(self, fp, assignment):
+        """Associate a file (binary file object) with an assignment.
+
+        Args:
+            fp: A binary file-like object with the .read() method.
+            assignment (gurutracker.database.objects.Assignment):
+                The assignment to associate `fp` with.
+
+        Raises:
+            gurutracker.storage.base.CannotLinkFileError:
+                When the file cannot be created.
+        """
+        pass
+
+    @abstractmethod
+    def has_linked_file(self, assignment):
+        """Check if file is linked to the assignment.
+
+        Args:
+            assignment (gurutracker.database.objects.Assignment):
+                The assignment to check.
+        """
+        pass
+
+    @abstractmethod
+    def get_file(self, assignment):
+        """Get the file which is linked to the assignment.
+
+        Args:
+            assignment (gurutracker.database.objects.Assignment):
+                The assignment whose files are to be got.
+
+        Raises:
+            gurutracker.storage.base.CannotGetFileError:
+                When the file cannot be got.
+        """
+        pass
+
+    @abstractmethod
+    def unlink_file(self, assignment):
+        """Unlink all files related to assignment.
+
+        Args:
+            assignment (gurutracker.database.objects.Assignment):
+                The assignment whose files are to be unlinked.
+
+        Raises:
+            gurutracker.storage.base.CannotUnlinkFileError:
+                When the file cannot be created.
+        """
+        pass
+
+    @abstractmethod
+    def get_all(self, dirpath):
+        """Put all files in a folder, and each file should be named as
+        `file_{fileid:04d}`
+
+        Args:
+            dirpath (str):
+                The directory to put all the files into.
+        """
+        pass
---
+++ gurutracker/storage/filesystem.py
@@ -0,0 +1,64 @@
+from gurutracker.storage.base import Base
+
+class Filesystem(Base):
+    def __init__(self, config):
+        pass
+    
+    def link_file(self, fp, assignment):
+        """Associate a file (binary file object) with an assignment.
+
+        Args:
+            fp: A binary file-like object with the .read() method.
+            assignment (gurutracker.database.objects.Assignment):
+                The assignment to associate `fp` with.
+
+        Raises:
+            gurutracker.storage.base.CannotLinkFileError:
+                When the file cannot be created.
+        """
+        pass
+
+    def has_linked_file(self, assignment):
+        """Check if file is linked to the assignment.
+
+        Args:
+            assignment (gurutracker.database.objects.Assignment):
+                The assignment to check.
+        """
+        pass
+
+    def get_file(self, assignment):
+        """Get the file which is linked to the assignment.
+
+        Args:
+            assignment (gurutracker.database.objects.Assignment):
+                The assignment whose files are to be got.
+
+        Raises:
+            gurutracker.storage.base.CannotGetFileError:
+                When the file cannot be got.
+        """
+        pass
+
+    def unlink_file(self, assignment):
+        """Unlink all files related to assignment.
+
+        Args:
+            assignment (gurutracker.database.objects.Assignment):
+                The assignment whose files are to be unlinked.
+
+        Raises:
+            gurutracker.storage.base.CannotUnlinkFileError:
+                When the file cannot be created.
+        """
+        pass
+
+    def get_all(self, dirpath):
+        """Put all files in a folder, and each file should be named as
+        `file_{fileid:04d}`
+
+        Args:
+            dirpath (str):
+                The directory to put all the files into.
+        """
+        pass
