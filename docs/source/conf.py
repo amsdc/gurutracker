67036fa0f5e5f78e0d1bc2f82acb9addf9acf7ba
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2023-03-20 18:55:28 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,5 +43,7 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
+html_theme_options = {
+    'navigation_depth': 3,
+}
 # sphinx-apidoc -o ./docs/source .
